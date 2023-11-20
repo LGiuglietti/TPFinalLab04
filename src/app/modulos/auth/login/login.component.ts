05a57305ee7dc265b67e917171a2983911d27a78
echeverriaalex <aleex.naahuel@outlook.com>
2023-11-20 13:33:02 -0300

    aplico validators, arreglos de css y traduccion del navbar

--- src/app/modulos/auth/login/login.component.ts
+++ src/app/modulos/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/core/Models';
 import { ApiService } from 'src/app/core/services/api.service';
@@ -10,35 +11,61 @@ import { UserService } from 'src/app/core/services/user.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
-  public user: User=new User();
-  constructor(private authService: AuthService, private router: Router, private userService: UserService){}
+  public user: User = new User();
 
-  ngOnInit(): void {
-    
-  }
+  form: FormGroup = this.formBuilder.group({
+    email: ["", [Validators.required, Validators.minLength(5), Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(8)]]
+  })
 
-  public async userLogin()
-  {
-      try{
-        const check= await this.authService.checkAuth(this.user.email,this.user.password);
-        if(check.length>0)
-        {
+  constructor(private authService: AuthService, private router: Router, private userService: UserService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void { }
+
+  public async userLogin() {
+    console.log(this.form.value);
+
+    if (this.form.invalid) {
+      alert("formulario invalido NO cumple con todo los validators")
+      return;
+    }
+
+    if (this.form.valid) {
+      alert("formulario valido cumple con todo los validators")
+      this.user.email = this.form.get("email")?.value
+      this.user.password = this.form.get("password")?.value
+
+      //this.user.email = this.form.controls["email"].value
+      //this.user.password = this.form.controls["password"].value
+
+      console.log(this.user);
+
+      try {
+        const check = await this.authService.checkAuth(this.user.email, this.user.password);
+        if (check.length > 0) {
           console.log("home");
-          this.user=check[0];
+          this.user = check[0];
           this.userService.setSessionUser(this.user);
           this.router.navigate(["/main"]);
         }
-        else{
+        else {
           throw new Error;
-          
+
         }
-      }catch(error){
+      } catch (error) {
         alert("usuario inexistente");
       }
-      
+    }
+
   }
 
+  public goHome() {
+    this.router.navigate(["/landing"]);
+  }
 
+  public goRegister() {
+    this.router.navigate(["/auth/register"]);
+  }
 }
--- src/app/modulos/auth/register/register.component.ts
+++ src/app/modulos/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/core/Models';
 import { ApiService } from 'src/app/core/services/api.service';
@@ -9,25 +10,70 @@ import { ApiService } from 'src/app/core/services/api.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+
   public user: User = new User();
-  ngOnInit(): void {
-  }
-  constructor(private apiService: ApiService, private router: Router) { }
+
+  
+
+  form: FormGroup = this.formBuilder.group({
+    username: ["", [Validators.required, Validators.minLength(3)]],
+    email: ["", [Validators.required, Validators.minLength(5), Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(8)]],
+    repeatpassword: ["", [Validators.required, Validators.minLength(8)]]
+  })
+  
+  message = "";
+
+  ngOnInit(): void { }
+
+  constructor(private apiService: ApiService, private router: Router, private formBuilder: FormBuilder) { }
 
   public userRegister() {
-    this.apiService.setUser(this.user).subscribe({
-      next: () => {
-        alert("usuario creado");
-        this.router.navigate(["./auth/login"]);
-      },
-      error: (error) => {
-        console.error(error)
-        if (this.user.email == '' || this.user.password == '' || this.user.userName == '') {
-          alert("todavia hay campos en blanco");
-        } else {
-          alert("mail ya en uso");
-        }
+
+    console.log(this.form.value);
+
+    if (this.form.invalid) {
+      alert("formulario invalido NO cumple con todo los validators")
+      return;
+    }
+
+    if(this.form.valid){
+      alert("formulario valido cumple con todo los validators")
+
+      if(this.form.get("password")?.value == this.form.get("repeatpassword")?.value ){
+
+        alert("Las contraseñas son iguales FELICIDADES")
+        
+        this.user.email = this.form.get("email")?.value
+        this.user.userName = this.form.get("username")?.value
+        this.user.password = this.form.get("password")?.value
+      
+        this.apiService.setUser(this.user).subscribe({
+          next: () => {
+            alert("usuario creado");
+            this.router.navigate(["./auth/login"]);
+          },
+          error: (error) => {
+            console.error(error)
+            if (this.user.email == '' || this.user.password == '' || this.user.userName == '') {
+              alert("todavia hay campos en blanco");
+            } else {
+              alert("mail ya en uso");
+            }
+          }
+        })
       }
-    })
+      else{
+        this.message = "Make sure you repeat the exact same password in both fields"
+      }
+    }
+  }
+
+  public goHome(){
+    this.router.navigate(["/landing"]);
+  }
+
+  public goLogin(){
+    this.router.navigate(["/auth/login"]);
   }
 }
